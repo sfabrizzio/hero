7ced929ae14718e94dc11bcba374ba1c721a75cf
sfabrizzio <sfabrizzio@hotmail.com>
2016-09-11 12:15:44 -0500

    commit

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -30,28 +30,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
-    private RecordRepository repository;
+   
 
-    @Autowired
-    public HomeController(RecordRepository repository) {
-        this.repository = repository;
-    }
+    
 
     @RequestMapping(method = RequestMethod.GET)
     public String home(ModelMap model) {
-        List<Record> records = repository.findAll();
-        model.addAttribute("records", records);
-        model.addAttribute("insertRecord", new Record());
+        
+      
+       
         return "home";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String insertData(ModelMap model, 
-                             @ModelAttribute("insertRecord") @Valid Record record,
-                             BindingResult result) {
-        if (!result.hasErrors()) {
-            repository.save(record);
-        }
-        return home(model);
-    }
+    
 }
